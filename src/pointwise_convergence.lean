9fb05e498e19bca597f48c1d0beef1c904dd4c33
amesnard0 <8011602+amesnard0@users.noreply.github.com>
2021-04-28 21:13:09 +0200

    Add files via upload

--- src/pointwise_convergence.lean
+++ src/pointwise_convergence.lean
@@ -17,6 +17,7 @@ def pointwise_lim {X Y : Type} [topological_space Y] (f : ℕ → (X → Y)) (F 
 instance (X Y : Type) [topological_space Y] : topological_space (X → Y) :=
 topological_space.generate_from (X → Y) {U | ∃ (x : X) (Ux : set Y) (hUx : is_open Ux), U = {f : X → Y | f x ∈ Ux}}
 
+-- Base de la topologie produit (intersections finies de générateurs) :
 lemma is_open_fnct_iff {X Y : Type} [topological_space Y] {s : set (X → Y)} :
 is_open s ↔ ∀ f ∈ s, ∃ (D : set X) (φ : X → set Y) (hD : finite D) (hφ : ∀ x ∉ D, φ x = univ), (∀ x, is_open (φ x)) ∧ (∀ x, f x ∈ φ x) ∧ ({f' : X → Y | ∀ x, f' x ∈ φ x} ⊆ s) :=
 begin
@@ -83,11 +84,73 @@ begin
         rw hφ_.2.1 x hx2, simp, }, },
     rw clef2,
     apply finite_inter,
-    { sorry },
+    { let ψ := λ x, {f' : X → Y | f' x ∈ φ_ f hf x},
+      have : {_x : set (X → Y) | ∃ (x : X) (H : x ∈ D_ f hf), {f' : X → Y | f' x ∈ φ_ f hf x} = _x} = ψ '' (D_ f hf),
+      { apply le_antisymm,
+        { rintros V ⟨x, hx, hV⟩, rw ← hV, clear hV V,
+          use x, split, exact hx, refl, },
+        { rintros V ⟨x, hx, hV⟩, rw ← hV, clear hV V,
+          use x, split, exact hx, refl, }, },
+      rw this,
+      exact finite.image ψ hφ_.1, },
     rintros Vx ⟨ x, hx, hVx ⟩,
     rw ← hVx,
     apply generated_open.generator,
     use [x, φ_ f hf x], split,
     exact hφ_.2.2.1 x,
     refl, },
+end
+
+-- Cette topologie est la topologie de la convergence simple :
+example {X Y : Type} [topological_space Y] (f : ℕ → (X → Y)) (F : X → Y) :
+pointwise_lim f F ↔ seq_lim f F :=
+begin
+  split,
+  { intro hyp,
+    rintros V ⟨U, hU, hFU, hUV⟩,
+    rw is_open_fnct_iff at hU,
+    rcases hU F hFU with ⟨D, φ, hD, hφ, h1, h2, h3⟩,
+    cases em (D.nonempty) with hD1 hD2,
+    { have clef : ∀ x, ∃ (N : ℕ), ∀ n ≥ N, f n x ∈ φ x,
+      { intros x,
+        have hφx : neighbourhood (F x) (φ x),
+        { use φ x, repeat {split},
+          exact h1 x, exact h2 x, refl, },
+        exact hyp x (φ x) hφx, },
+      choose ψ hψ using clef,
+      cases finite.exists_maximal_wrt ψ D hD hD1 with x₀ hx₀, rw exists_prop at hx₀,
+      let N := ψ x₀,
+      have hN : ∀ x ∈ D, N ≥ ψ x,
+      { intros x hx,
+        cases le_total N (ψ x) with H1 H2,
+        exact ge_of_eq (hx₀.2 x hx H1),
+        exact H2, },
+      use N,
+      intros n hn,
+      apply hUV, apply h3,
+      intro x,
+      cases em (x ∈ D) with hx1 hx2,
+      exact hψ x n (ge_trans hn (hN x hx1)),
+      rw hφ x hx2, simp, },
+    { rw nonempty_def at hD2, push_neg at hD2,
+      use 0,
+      intros n hn,
+      apply hUV, apply h3,
+      intro x, rw hφ x (hD2 x), simp, }, },
+  { intro hyp,
+    rintros x Vx ⟨Ux, hUx, hxUx, hUxVx⟩,
+    let V := {g : X → Y | g x ∈ Vx},
+    have hV: neighbourhood F V,
+    { use {g : X → Y | g x ∈ Ux}, repeat {split},
+      apply generated_open.generator,
+      use [x, Ux], split,
+      exact hUx,
+      refl,
+      exact hxUx,
+      intros g hg,
+      exact hUxVx hg, },
+    cases hyp V hV with N hN,
+    use N,
+    intros n hn,
+    exact hN n hn, },
 end
